52b2404ca457367a35527dbc9a1a8a6ee526984b
subrasub <s35subra@edu.uwaterloo.ca>
2018-08-21 15:17:52 -0400

    Robot Return

---
+++ Leetcode/Q657.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0, y=0;
+    
+        for(int i=0; i<moves.size(); ++i){
+            switch(moves[i]){
+                case 'U':
+                    ++y; break;
+                case 'D':
+                    --y; break;
+                case 'R':
+                    ++x; break;
+                case 'L':
+                    --x; break;
+            }
+        }
+        
+        return (x==0 && y==0);
+            
+    }
+};
