12d49a000aadcd7dadeded33c4e263554e1223fe
subrasub <s35subra@edu.uwaterloo.ca>
2018-09-14 18:15:50 -0400

    Reverse Integer (97.2%)

--- Leetcode/7.cpp
+++ Leetcode/7.cpp
@@ -5,6 +5,7 @@ public:
         int res=0; 
         while(x!=0){
             int d = x%10;
+            // accounting for the integer overflow
             if(res > INT_MAX/10)
                 return 0;
             if(res < INT_MIN/10)
