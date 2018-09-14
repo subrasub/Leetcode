5e6d4e82350a4838b3ce7f6dde42348e17522850
subrasub <s35subra@edu.uwaterloo.ca>
2018-09-14 18:14:22 -0400

    account for overflow

---
+++ Leetcode/7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int res=0; 
+        while(x!=0){
+            int d = x%10;
+            if(res > INT_MAX/10)
+                return 0;
+            if(res < INT_MIN/10)
+                return 0;
+            res = 10*res + d;
+            x/=10;
+        }
+        return res;
+    }
+};
