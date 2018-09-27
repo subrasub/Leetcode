c07f7a8f6fd4ffd1d4a3d08b5e58385a564930de
subrasub <s35subra@edu.uwaterloo.ca>
2018-09-27 13:31:19 -0400

    Valid Paranthesis

--- Leetcode/20.cpp
+++ Leetcode/20.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int para = 0;
-        // int p_right = 0;
-        int sq = 0;
-        // int s_right = 0;
-        int curl = 0;
-        // int c_right = 0;
-        
-        for(int i=0; i< s.size(); i++){
-            if(s[i]=='(')
-                para = para + 1;
-            else 
-            if(s[i] == ')')
-                para = para - 1;
-            else
-            if(s[i] == '{')
-                curl = curl + 1;
-            else
-            if(s[i] == '}')
-                curl = curl - 1;
-            else
-            if(s[i] == '[')
-                sq = sq + 1;
-            else
-            if(s[i] == ']')
-                sq = sq - 1;
-            
-            cout<<s[i];
-            cout<<"\npara: "<<para<<endl;
-            cout<<"curl: "<<curl<<endl;
-            cout<<"sq: "<<sq<<endl;
-        }
-        
-        if(para == 0 && curl == 0 && sq == 0)
+        if(s.size()==0)
             return true;
-        return false;
+        if(s.size()%2 != 0)
+            return false;
+        
+        stack <char> res;
+        
+        map<char,char> par;
+        par[')'] = '(';
+        par[']'] = '[';
+        par['}'] = '{';
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '{' || s[i] == '(' || s[i] == '[')
+                res.push(s[i]);
+            if(s[i] == '}' || s[i] == ')' || s[i] == ']'){
+                if(res.empty() || res.top() != par[s[i]])
+                    return false;
+                else
+                    res.pop();
+            }
+        }
+        return res.empty()? true: false;
     }
 };
