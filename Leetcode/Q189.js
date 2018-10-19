f0016eed05cec05b34e25871311f4ebbb7688be2
subrasub <s35subra@edu.uwaterloo.ca>
2018-10-18 22:07:22 -0400

    Rotate array in place (using unshift() and splice())

--- Leetcode/Q189.js
+++ Leetcode/Q189.js
@@ -4,20 +4,8 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    
-    k%=nums.length
-    
-    function r_inplace(nums, l, r){
-        while(l<=r){
-            var temp = nums[l]
-            nums[l] = nums[r]
-            nums[r] = temp
-            l++
-            r--
-        }
-        return nums
-    }
-    r_inplace(nums, 0, nums.length-1)
-    r_inplace(nums, 0, k-1)
-    r_inplace(nums, k , nums.length-1)
+    //Didn't know you could do this in JS, wow!
+
+    var shift = nums.splice(nums.length-k)
+    nums.unshift(... shift)
 };
