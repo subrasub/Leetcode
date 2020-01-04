36b6cd2dad9c10806d6a5703a5a8001a8a9602a4
Subramanian <s35subra@edu.uwaterloo.ca>
2020-01-04 16:55:05 -0500

    Maximum Width of a Binary Tree (Medium)

---
+++ Leetcode/Q662.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 0
+        left = {}
+        
+        def searchDFS(node, depth = 0, position = 0):
+            if node:
+                left.setdefault(depth, position)
+                self.ans = max(self.ans, position-left[depth]+1)
+                searchDFS(node.left, depth+1, position*2)
+                searchDFS(node.right, depth+1, position*2+1)
+        
+        searchDFS(root)
+        return self.ans
+                
