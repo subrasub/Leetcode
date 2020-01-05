69c302326a69c9d2a4d45f67ca05e662cb6be849
Subramanian <s35subra@edu.uwaterloo.ca>
2020-01-04 23:10:24 -0500

    Path Sum (Easy)

---
+++ Leetcode/Q112.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        else:
+            if root.left is None and root.right is None and root.val==sum:
+                return True
+            
+            sum -= root.val
+            
+            return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
+                
+      
+                
