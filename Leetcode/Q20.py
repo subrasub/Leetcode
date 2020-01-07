c44beb362396c2e324d25537f58ed5d63c307df2
Subramanian <s35subra@edu.uwaterloo.ca>
2020-01-06 23:27:53 -0500

    Valid Parantheses (Easy)

---
+++ Leetcode/Q20.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s:
+            return True
+        
+        if len(s)%2 != 0:
+            return False
+        
+        paran = {'(': ')', '[': ']', '{': '}'}
+        stack = []
+        
+        for i in s:
+            if i in paran:
+                stack.append(paran[i])
+                
+            else:
+                if not stack or stack.pop() != i:
+                    return False
+            
+        return len(stack)==0
