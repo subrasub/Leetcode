09e6c0c1b163d492cc3af97f5af8810f81eee491
Subramanian <s35subra@edu.uwaterloo.ca>
2020-01-07 00:02:42 -0500

    Largest Perimeter Triangle (Easy)

---
+++ Leetcode/Q976.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestPerimeter(self, A: List[int]) -> int:
+        if not A: 
+            return 0
+        
+        A.sort()
+        A = A[::-1]
+        
+        for i in range(2, len(A)):
+            if A[i] + A[i-1] > A[i-2]:
+                return A[i]+A[i-1]+A[i-2]
+        return 0
+            
