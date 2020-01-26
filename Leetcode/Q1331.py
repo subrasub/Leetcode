418313809ea7f3222fe4daab9ab30014e58bbd65
Subramanian <s35subra@edu.uwaterloo.ca>
2020-01-25 23:18:22 -0500

    Rank Transform of an Array

---
+++ Leetcode/Q1331.py
@@ -0,0 +1,12 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        table = {}
+        res = []
+        
+        for i in sorted(arr):
+            table.setdefault(i, len(table)+1)
+        
+        for i in arr:
+            res.append(table[i])
+            
+        return res
