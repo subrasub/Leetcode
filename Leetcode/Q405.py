30ac118e39b3ebae4a32403fa83056e201912c84
Subramanian <s35subra@edu.uwaterloo.ca>
2020-02-19 21:30:55 -0500

    Convert a number to hexadecimal

---
+++ Leetcode/Q405.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/convert-a-number-to-hexadecimal
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num==0:
+            return '0'
+        
+        table = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f'}     
+        res = []
+        s = ""
+        
+        if num<0:
+            num += 2**32
+            
+        while(num != 0):
+            r = num % 16
+            res.append(r)
+            num = num // 16
+        
+        
+        for i in res[::-1]:
+            
+            if i in table:
+                s+=table[i]
+        
+        return s
