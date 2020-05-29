be9f870193d3e8db848a5104a2dd259eedd5ee86
Subramanian <s35subra@edu.uwaterloo.ca>
2020-05-29 11:14:26 -0400

    Find the Town Judge

---
+++ Leetcode/Q997.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/find-the-town-judge/
+
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        table = [0]*(N+1)
+        
+        for x,y in trust: 
+            table[x] -= 1
+            table[y] += 1
+        
+        for i in range(1, N+1):
+            if table[i] == N-1:
+                return i
+        return -1
