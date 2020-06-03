1b1866ad13cae55296ffd55e2c2cb93fa8c346a6
Subramanian <s35subra@edu.uwaterloo.ca>
2020-06-03 12:39:40 -0400

    Two City Scheduling

---
+++ Leetcode/Q1029.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/two-city-scheduling/
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        res = 0
+        
+        # Sort according to the absolute difference of the values
+        costs.sort(key = lambda x: x[0]-x[1])
+        print(costs)
+        
+        N = len(costs)//2
+        
+        for x,y in costs:
+            if N > 0:
+                res += x
+                print(x)
+            else:
+                res += y
+                print(y)   
+            N -= 1
+             
+        return res
