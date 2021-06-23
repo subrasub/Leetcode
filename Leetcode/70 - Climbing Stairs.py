fdfcefaaeef1b7d74c075428a531080e3eb37e8e
Subramanian <s35subra@edu.uwaterloo.ca>
2021-06-22 23:21:14 -0400

    update

--- Leetcode/70 - Climbing Stairs.py
+++ Leetcode/70 - Climbing Stairs.py
@@ -6,31 +6,29 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n==1:
             return 1
-        
+
         dp = [0]*(n+1)
         dp[1] = 1
         dp[2] = 2
-        
+
         for i in range(3, n+1):
             dp[i] = dp[i-1] + dp[i-2]
-        
+
         return dp[n]
-        
+
     # recursive way
+    class Solution:
     def climbStairs(self, n: int) -> int:
-        def helper(i, n, memo):
-            if i>n:
+        def climbFind(i, n):
+            if i > n:
                 return 0
-            
-            if i==n:
+            if i == n:
                 return 1
-            
-            if memo[i]:
-                return memo[i]
+            if self.dp[i] > 0:
+                return self.dp[i]
 
-            memo[i] = helper(i+1, n, memo) + helper(i+2, n, memo)
-            return memo[i]
+            self.dp[i] = climbFind(i+1, n) + climbFind(i+2, n)
+            return self.dp[i]
 
-        memo = [0]*n
-        return helper(0, n, memo)
-    
+        self.dp = [0 for _ in range(n+1)]
+        return climbFind(0, n)
