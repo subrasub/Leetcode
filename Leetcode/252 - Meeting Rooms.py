1c18cd735f91230163b5ae6b551d8be01eff4d99
Subramanian <s35subra@edu.uwaterloo.ca>
2021-07-29 14:37:35 -0400

    easy x1

--- Leetcode/252 - Meeting Rooms.py
+++ Leetcode/252 - Meeting Rooms.py
@@ -1,8 +1,8 @@
 class Solution:
-  def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-    intervals.sort()
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort(key = lambda x:x[0])
 
-    for i in range(len(intervals)-1):
-      if intervals[i][1] > intervals[i+1][0]:
-        return False
-    return True
+        for i in range(1, len(intervals)):
+            if intervals[i-1][1] > intervals[i][0]:
+                return False
+        return True
