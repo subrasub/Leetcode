775e433512ba04fe138466ae8808670489ee192b
Subramanian <s35subra@edu.uwaterloo.ca>
2021-11-08 21:04:09 -0500

    medium x1

--- Leetcode/91 - Decode Ways.py
+++ Leetcode/91 - Decode Ways.py
@@ -1,22 +1,15 @@
-# https://leetcode.com/problems/decode-ways/
-# Tags - String, Dynamic Programming
-
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0]=="0":
-            return 0
-        
-        count = [0]*(len(s)+1)
-        
-        count[0], count[1] = 1,1
-        
-        for i in range(2, len(s)+1):
-            count[i] = 0
-            
-            if(s[i-1] > '0'):
-                count[i] = count[i-1]
-            
-            if(s[i-2] == '1' or (s[i-2] == '2' and s[i-1]<'7')):
-                count[i] += count[i-2]
-        
-        return count[-1]
+        dp = [0 for _ in range(len(s)+1)]
+
+        dp[0] = 1
+        dp[1] = 0 if s[0]=='0' else 1
+
+        for i in range(2, len(dp)):
+            if s[i-1] != '0':
+                dp[i] = dp[i-1]
+
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[len(s)]
