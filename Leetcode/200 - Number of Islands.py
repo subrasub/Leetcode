64a27f113b6a3638cbb03c3abb7e0aafbda6c342
Subramanian <s35subra@edu.uwaterloo.ca>
2021-11-16 21:03:11 -0500

    medium x1

--- Leetcode/200 - Number of Islands.py
+++ Leetcode/200 - Number of Islands.py
@@ -1,27 +1,26 @@
 class Solution:
   def numIslands(self, grid: List[List[str]]) -> int:
     def dfs(grid, r, c):
-      grid[r][c]='0'
-      if((r-1) >= 0 and grid[r-1][c]=='1'):
+      grid[r][c] = '0'
+
+      if (r-1)>=0 and grid[r-1][c]=='1':
         dfs(grid, r-1, c)
-      if((r+1) < self.rows and grid[r+1][c]=='1'):
+      if (r+1)<self.rows and grid[r+1][c]=='1':
         dfs(grid, r+1, c)
-      if((c-1) >= 0 and grid[r][c-1]=='1'):
+      if (c-1)>=0 and grid[r][c-1]=='1':
         dfs(grid, r, c-1)
-      if((c+1) < self.columns and grid[r][c+1]=='1'):
+      if (c+1)<self.cols and grid[r][c+1]=='1':
         dfs(grid, r, c+1)
 
+
     self.rows = len(grid)
-    self.columns = len(grid[0])
-
-    if(not self.rows or not self.columns):
-      return 0
-
+    self.cols = len(grid[0])
     res = 0
 
     for r in range(self.rows):
-      for c in range(self.columns):
-        if(grid[r][c]=='1'):
+      for c in range(self.cols):
+        if grid[r][c]=='1':
           res += 1
           dfs(grid, r, c)
+
     return res
