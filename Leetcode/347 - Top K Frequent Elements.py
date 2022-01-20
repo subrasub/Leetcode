bb23735ff100c0b2a7b15437811047e7800ec0e6
Subramanian <s35subra@edu.uwaterloo.ca>
2022-01-20 15:03:32 -0500

    medium x1

--- Leetcode/347 - Top K Frequent Elements.py
+++ Leetcode/347 - Top K Frequent Elements.py
@@ -3,6 +3,5 @@
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = collections.Counter(nums)
-        # print(count)
+        count = Counter(nums)
         return heapq.nlargest(k, count.keys(), key = count.get)
