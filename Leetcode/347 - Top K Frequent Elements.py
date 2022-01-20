9233ecdda97745ec400b241118c4af21386217fc
Subramanian <s35subra@edu.uwaterloo.ca>
2022-01-20 15:10:12 -0500

    medium x1 update

--- Leetcode/347 - Top K Frequent Elements.py
+++ Leetcode/347 - Top K Frequent Elements.py
@@ -5,3 +5,17 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
         return heapq.nlargest(k, count.keys(), key = count.get)
+
+    def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
+        res = []
+
+        count = Counter(nums)
+
+        max_heap = [(-val, key) for key, val in count.items()]
+
+        heapq.heapify(max_heap)
+
+        for _ in range(k):
+            res.append(heapq.heappop(max_heap)[1])
+
+        return res
